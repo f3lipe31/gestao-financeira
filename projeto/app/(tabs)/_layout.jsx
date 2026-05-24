5ca8b412c2de4474e890497370107033b33d51b3
f3lipe31 <lipecunha2005@gmail.com>
2026-05-24 14:53:54 -0300

    Feat: Finaliza a implementação do Projeto 4 (UX e Componentização)

--- projeto/app/(tabs)/_layout.jsx
+++ projeto/app/(tabs)/_layout.jsx
@@ -1,67 +1,43 @@
-import { Tabs } from "expo-router"
-import { colors } from "../../constants/colors"
-import { MaterialIcons } from "@expo/vector-icons"
-import { StyleSheet, TouchableOpacity, View } from "react-native"
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function TabsLayout() {
+export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.primary },
-        headerTintColor: colors.primaryContrast,
-        headerTitleAlign: "center",
-        tabBarActiveTintColor: colors.primary,
-        tabBarInactiveTintColor: colors.inactive,
-        tabBarStyle: {
-            height: 60,
-            paddingTop: 5,
-            backgroundColor: colors.background
-        },
-        tabBarButton: (props) => <TouchableOpacity {...props} activeOpacity={0.8}/>
+    <Tabs 
+      screenOptions={{ 
+        headerShown: false, 
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: "#007AFF",
+        tabBarInactiveTintColor: "#8E8E93",
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Transações",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="attach-money" size={28} color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="add-transactions"
-        options={{
-          title: "Adicionar Transação",
-          tabBarLabel: "",
-          tabBarIcon: () => (
-            <View style={styles.addButton}>
-              <MaterialIcons name="add" size={40} color={colors.primaryContrast} />
-            </View>
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="summary"
-        options={{
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
           title: "Resumo",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="pie-chart" size={28} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="pie-chart" size={size} color={color} />
           )
-        }}
+        }} 
+      />
+      <Tabs.Screen 
+        name="add-transactions" 
+        options={{ 
+          title: "Adicionar",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle" size={size} color={color} />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="summary" 
+        options={{ 
+          title: "Extrato",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list" size={size} color={color} />
+          )
+        }} 
       />
     </Tabs>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  addButton: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    height: 64,
-    width: 64,
-    borderRadius: 32,
-    backgroundColor: colors.primary
-  }
-})
